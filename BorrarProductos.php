0dfa4fa81940b43318e132a87b922b882dbf6cdb
facundo94 <facu.31@live.com>
2017-09-25 15:33:12 -0300

    BorrarProductos listo

--- BorrarProductos.php
+++ BorrarProductos.php
@@ -4,4 +4,44 @@ si lo recibe por post con el parametro "que debo hacer?"  = borrar se debe borra
 al producto  mover la foto al subdirectorio productos borrados con el nombre formado por
 el producto y la fecha de borrado */
 
+include "Producto.php";
+
+$listaProducto = Producto::RetornarArrayDeProductos();
+$nombre;
+$accion;
+
+if(isset($_POST["nombre"])){
+
+    $nombre = $_POST["nombre"];
+    $accion = $_POST["queDeboHacer"];
+    $productoBorrado;
+
+    foreach($listaProducto as &$producto){
+        if($producto->getNombre() == $nombre && $accion == "borrar"){
+            $productoBorrado = array_search($producto, $listaProducto);
+            unset($listaProducto[$productoBorrado]);
+
+            $origen = "img/$nombre.png";
+            $destino = 'imagenesBorradas/';
+            copy($origen, $destino."$nombre.png");
+            unlink("img/$nombre.png");
+            break;
+        }
+    }
+
+    
+
+    
+
+}else if(isset($_GET["nombre"])){
+    $nombre = $_GET["nombre"];
+
+    foreach($listaProducto as &$producto){
+        if($producto->getNombre() == $nombre){
+            echo "El producto se encuentra en la lista!";
+            break;
+        }
+    }
+}
+
 ?>
